4717bb8eb4c1247319ddb95a28b298794223f67b
rashmir1196 <rashmi.raj@blueyonder.com>
2022-11-11 10:48:31 +0530

    New Changes

---
+++ src/main/java/com/spring/crud/mongo/springCrudMongoWithJunit/exceptionHandling/ErrorResponse.java
@@ -0,0 +1,24 @@
+package com.spring.crud.mongo.springCrudMongoWithJunit.exceptionHandling;
+
+public class ErrorResponse {
+
+    private int errorCode;
+    private String message;
+
+
+    public int getErrorCode(){
+        return this.errorCode;
+    }
+
+    public void setErrorCode(int errorCode){
+        this.errorCode = errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/spring/crud/mongo/springCrudMongoWithJunit/exceptionHandling/MovieException.java
@@ -0,0 +1,20 @@
+package com.spring.crud.mongo.springCrudMongoWithJunit.exceptionHandling;
+
+public class MovieException extends Exception{
+
+        private static final long serialVersionUID = 1L;
+
+        private String errorMessage;
+
+        public MovieException(){}
+
+        public MovieException(String errorMessage){
+            super(errorMessage);
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorMessage(){
+            return this.errorMessage;
+        }
+
+}
